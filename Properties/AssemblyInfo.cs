49cee2b30926ab57ed33c3a10ecdd67899bdc9dd
ChrisHammond <github@christoc.com>
2015-02-27 00:13:34 -0600

    Updates for 2.0.0 release, updated copyright year

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -1,5 +1,5 @@
 ﻿/*
-' Copyright (c) 2014 Christoc.com Software Solutions
+' Copyright (c) 2015 Christoc.com Software Solutions
 '  All rights reserved.
 ' 
 ' Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated 
@@ -29,7 +29,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("dnnCHAT")]
-[assembly: AssemblyCopyright("Copyright © Christoc.com 2014")]
+[assembly: AssemblyCopyright("Copyright © Christoc.com 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -50,5 +50,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("01.01.02.*")]
-[assembly: AssemblyFileVersion("01.01.02.*")]
+[assembly: AssemblyVersion("02.00.00.*")]
+[assembly: AssemblyFileVersion("02.00.00.*")]
